faadbf8895ad493c6bfbc04d61169a76a7d4aa31
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-11-22 20:52:04 +0000

    update

--- test_script.R
+++ test_script.R
@@ -1,7 +1,8 @@
+#.rs.restartR() # Restarting R session...
 #install.packages("devtools")
+#remove.packages("simuloR", lib="~/R/x86_64-pc-linux-gnu-library/3.6")
 #devtools::install_github("nanhung/simuloR")
 library(simuloR)
-simuloR::mcsim_download()
 
 file <- "models/perc.model"
 makemcsim(file)
