0feaa2aebf2283f69fca2dcfe1cb67e3c25c0157
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-11-22 18:28:25 -0600

    add CC0

---
+++ Packages.R
@@ -0,0 +1,12 @@
+# Please install all of the following packages prior to the workshop:
+
+################################################
+###               Packages                   ###
+################################################
+
+install.packages("devtools", dependencies = TRUE)
+install.packages("rstan", dependencies = TRUE)
+install.packages("bayesplot", dependencies = TRUE)
+install.packages("tidyverse", dependencies = TRUE)
+install.packages("pksensi", dependencies = TRUE)
+devtools::install_github("nanhung/simuloR")
