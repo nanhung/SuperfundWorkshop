01f4866c3be873729179023588328caa17453d07
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-11-23 08:09:44 -0600

    update

--- Packages.R
+++ Packages.R
@@ -4,9 +4,9 @@
 # Please install all of the following packages prior to the workshop. 
 
 pkgs <- c("devtools", 
-          "rstan", 
           "bayesplot", 
           "tidyverse",
-          "pksensi")
+          "pksensi",
+          "rstan")
 install.packages(pkgs, dependencies = TRUE)
 devtools::install_github("nanhung/simuloR")
