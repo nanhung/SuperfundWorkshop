9a2767091299024b31788bdc071e9450da21d6fc
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-11-29 13:59:38 -0600

    test mtc, mcmc, and setpoints

--- examples/test_script.R
+++ examples/test_script.R
@@ -3,6 +3,19 @@
 #devtools::install_github("nanhung/simuloR")
 library(simuloR)
 
-model <- "models/simple.model"
-input <- "inputs/simple.in"
-df <- mcsim(model, input)
+model <- "models/linear.model"
+input <- "inputs/linear.in"
+mcsim(model, input)
+
+out <- read.delim("sim.out", skip = 2)
+plot(x = out$Time, y = out$y)
+abline(a = 1, b = 2)
+
+input <- "inputs/linear.mtc.in"
+mcsim(model, input)
+
+input <- "inputs/linear.mcmc.in"
+mcsim(model, input)
+
+input <- "inputs/linear.setpts.in"
+mcsim(model, input)
