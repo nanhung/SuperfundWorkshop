6eca4dd3b9bc20b8616a46ff9a0dcc7b0f4bc501
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-12-02 10:44:00 -0600

    preset

--- preset.R
+++ preset.R
@@ -2,7 +2,21 @@
 #.rs.restartR() # Restarting R session...
 #setwd("/cloud/project")
 #remove.packages("simuloR")
-#devtools::install_github("nanhung/simuloR")
+
+# Packages --------------------------------------------------------------
+
+# The installation will take a while. Do not do this at the last minute.
+# Please install all of the following packages prior to the workshop. 
+
+pkgs <- c("devtools", 
+          "bayesplot", 
+          "tidyverse",
+          "pksensi",
+          "rstan")
+install.packages(pkgs, dependencies = TRUE)
+devtools::install_github("nanhung/simuloR")
+
+# Testing --------------------------------------------------------------
 
 library(simuloR)
 
